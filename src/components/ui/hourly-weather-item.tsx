ebe6f51d7afb37d79051d0ad26f58cd3cd31dec0
zoe <183669693+Sikzyo@users.noreply.github.com>
2026-03-31 14:25:03 -0500

    feat: display sunrise and sunset times in hourly list

--- src/components/ui/hourly-weather-item.tsx
+++ src/components/ui/hourly-weather-item.tsx
@@ -8,6 +8,8 @@ interface HourlyWeatherItemProps {
   temperature: number;
   weatherCode: number;
   isDay: 0 | 1;
+  isSunrise?: true;
+  isSunset?: true;
 }
 
 export function HourlyWeatherItem({
@@ -15,12 +17,28 @@ export function HourlyWeatherItem({
   temperature,
   weatherCode,
   isDay,
+  isSunrise,
+  isSunset,
 }: HourlyWeatherItemProps) {
-  const WeatherIcon = getWeatherIcon(weatherCode, isDay === 1);
+  const WeatherIcon = getWeatherIcon(
+    weatherCode,
+    isDay === 1,
+    false,
+    isSunrise,
+    isSunset,
+  );
 
   return (
     <article className="flex flex-col items-center gap-2 md:gap-4">
-      <span className="font-manrope font-semibold">{temperature}°</span>
+      {!isSunrise && !isSunset && (
+        <span className="font-manrope font-semibold">{temperature}°</span>
+      )}
+      {isSunrise && (
+        <span className="font-manrope font-semibold">Amanecer</span>
+      )}
+      {isSunset && (
+        <span className="font-manrope font-semibold">Atardecer</span>
+      )}
       {createElement(WeatherIcon, { className: cn("w-6 h-6") })}
       <time className="font-manrope font-semibold text-nowrap">
         {formatHour(time)}
